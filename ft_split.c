c38ae8a5d11cd90227da79d35086b38093c026ab
Luciano Lima da Silva <luciano.lima.s@hotmail.com>
2022-06-03 22:12:01 -0300

    Correção ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,108 +6,73 @@
 /*   By: luciano <luciano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 15:03:26 by llima-da          #+#    #+#             */
-/*   Updated: 2022/05/30 14:46:44 by luciano          ###   ########.fr       */
+/*   Updated: 2022/06/03 22:07:12 by luciano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int	tot(char const *s, char c)
+static	size_t tot(char const *s, char c)
 {
-	size_t	total;
-	int		i;
-
-	i = 0;
-	total = 0;
-	while (s[i] != '\0')
-	{
-		while (s[i] == c)
-			i++;
-		if (i > 0 && s[i] && s[i - 1] == c)
-			total++;
-		if (s[i])
-			i++;
-	}
-	if (total == 0 && s[i - 1] == c)
-		return (0);
-	if (s[0] != c)
-		total++;
-	return (total);
-}
-
-static	char	**mal(char **ptr, char const *s, char c)
-{
-	size_t	total;
-	int		i;
-	int		j;
-
-	total = 0;
-	i = 0;
-	j = 0;
-	while (s[j++])
-	{
-		if (s[j] != c)
-			total++;
-		else if (j > 0 && s[j - 1] != c)
-		{
-			ptr[i] = malloc(sizeof(char) * (total + 1));
-			if (!ptr[i])
-				return (0);
-			total = 0;
-			i++;
-		}
-		if (s[j + 1] == '\0' && s[j] != c)
-		{
-			ptr[i] = malloc(sizeof(char) * total + 1);
-		}
-	}
-	return (ptr);
-}
-
-static char	**split(char **ptr, char const *s, char c)
-{
-	int	i;
-	int	j;
-	int	k;
+	size_t i;
+	size_t j;
 
 	i = 0;
 	j = 0;
-	k = 0;
-	while (s[k])
+	while (*s)
 	{
-		if (s[k] != c)
-			ptr[i][j++] = s[k];
-		else if (k > 0 && s[k - 1] != c)
+		if (*s != c && j == 0)
 		{
-			if (k != 0)
+			j = 1;
+			i++;
+		}
+		else if (*s == c)
+			j = 0;
+		s++;
+	}
+	return (i);
+}
+
+static size_t    word_len(char const *s, char sep)
+{
+    size_t    len;
+
+    len = 0;
+    while (s[len] && s[len] != sep)
+        len++;
+    return (len);
+}
+
+static char	**split(char **ptr, char const *s, char c, size_t total)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	while (i < total)
+		{
+			while (*s && *s == c)
+				s++;
+			if (*s)
 			{
-				ptr[i][j] = '\0';
-				j = 0;
-				i++;
+				len = word_len(s, c);
+				ptr[i++] = ft_substr(s, 0, len);
 			}
+			s += len;
 		}
-		if (s[k + 1] == '\0' && s[k] != c)
-			ptr[i][j] = '\0';
-		k++;
-	}
 	return (ptr);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**ptr;
-	int		total;
+	size_t		total;
 
 	total = tot(s, c);
-	ptr = malloc(sizeof(char *) * (total + 1));
+	ptr = malloc(sizeof(char *) * total + 1);
 	if (!ptr)
 		return (0);
-	if (mal(ptr, s, c) != 0)
-		split(ptr, s, c);
-	else
-	{
-		free(ptr);
-		return (NULL);
-	}
+	ptr[total] = (NULL);
+	split(ptr, s, c, total);
 	return (ptr);
 }
